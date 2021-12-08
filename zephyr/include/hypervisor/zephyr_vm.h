65887e08107e755c6d05a4c0f7d68cd3de7c5fdf
Suqier <suqier5637@foxmail.com>
2021-12-08 17:07:41 +0800

    Start to port vm's struct.
    Signed-off-by: Suqier <suqier5637@foxmail.com>

---
+++ zephyr/include/hypervisor/zephyr_vm.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Huang Jiajia
+ * Email: Huangjj2020@hnu.edu.cn
+ * Date: 2021-12-08
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#ifndef ZEPHYR_VM_H__
+#define ZEPHYR_VM_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* ZEPHYR_VM_H__ */
---
+++ zephyr/subsys/hypervisor/virt/zephyr_vm.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021 Huang Jiajia
+ * Email: Huangjj2020@hnu.edu.cn
+ * Date: 2021-12-08
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <hypervisor/hypervisor.h>
+#include <spinlock.h>   
+
+
+
+
+
+
+
+
+
+
