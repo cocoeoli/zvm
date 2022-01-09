086f0f88697f7e319c5fe18e58b04eb590ae7496
cocoeoli <1981639884@qq.com>
2022-01-09 20:50:30 +0800

    add some el2 register macro in zvm_arm.h

--- zephyr/include/arch/arm64/zvm_arm.h
+++ zephyr/include/arch/arm64/zvm_arm.h
@@ -11,30 +11,60 @@
 
 #include <sys/util.h>           //BIT()
 
-#define HCR_API		    BIT(41)
-#define HCR_RW		(1 << 31)
-
-
-/*
- * The bits we set in HCR:
- * TLOR:	Trap LORegion register accesses
- * RW:		64bit by default, can be overridden for 32bit VMs
- * TACR:	Trap ACTLR
- * TSC:		Trap SMC
- * TSW:		Trap cache operations by set/way
- * TWE:		Trap WFE
- * TWI:		Trap WFI
- * TIDCP:	Trap L2CTLR/L2ECTLR
- * BSU_IS:	Upgrade barriers to the inner shareable domain
- * FB:		Force broadcast of all maintenance operations
- * AMO:		Override CPSR.A and enable signaling with VA
- * IMO:		Override CPSR.I and enable signaling with VI
- * FMO:		Override CPSR.F and enable signaling with VF
- * SWIO:	Turn set/way invalidates into set/way clean+invalidate
- * PTW:		Take a stage2 fault if a stage1 walk steps in device memory
+/* hrc_el2 register bits 
+ * For hyp configure.
  */
+#define HCR_TID5	BIT(58) 				
+#define HCR_DCT		BIT(57)
+#define HCR_ATA		BIT(56) 
+#define HCR_AMVOFFEN	BIT(51)
+#define HCR_FIEN	BIT(47)
+#define HCR_FWB		BIT(46)
+#define HCR_API		BIT(41)
+#define HCR_APK		BIT(40)
+#define HCR_TEA		BIT(37)
+#define HCR_TERR	BIT(36)
+#define HCR_TLOR	BIT(35)
+#define HCR_E2H		BIT(34)
+#define HCR_ID		BIT(33)
+#define HCR_CD		BIT(32)
+#define HCR_RW		BIT(31)
+#define HCR_TRVM	BIT(30)
+#define HCR_HCD		BIT(29)
+#define HCR_TDZ		BIT(28)
+#define HCR_TGE		BIT(27)
+#define HCR_TVM		BIT(26)
+#define HCR_TTLB	BIT(25)
+#define HCR_TPU		BIT(24)
+#define HCR_TPC		BIT(23)
+#define HCR_TSW		BIT(22)
+#define HCR_TACR	BIT(21)
+#define HCR_TIDCP	BIT(20)
+#define HCR_TSC		BIT(19)
+#define HCR_TID3	BIT(18)
+#define HCR_TID2	BIT(17)
+#define HCR_TID1	BIT(16)
+#define HCR_TID0	BIT(15)
+#define HCR_TWE		BIT(14)
+#define HCR_TWI		BIT(13)
+#define HCR_DC		BIT(12)
+#define HCR_BSU		(3 << 10)
+#define HCR_BSU_IS	BIT(10)
+#define HCR_FB		BIT(9)
+#define HCR_VSE		BIT(8)
+#define HCR_VI		BIT(7)
+#define HCR_VF		BIT(6)
+#define HCR_AMO		BIT(5)
+#define HCR_IMO		BIT(4)
+#define HCR_FMO		BIT(3)
+#define HCR_PTW		BIT(2)
+#define HCR_SWIO	BIT(1)
+#define HCR_VM		BIT(0)
+#define HCR_RES0	(BIT(48) | BIT(39))
 
-#define HCR_HOST_NVHE_FLAGS (HCR_RW | HCR_API | HCR_APK | HCR_ATA)
+
+
+#define HCR_HOST_NVHE_FLAGS (HCR_RW | HCR_API | HCR_APK | HCR_ATA)  /* Host os NVHE flag */
 
 
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM64_TPIDRRO_EL0_H_ */
